1b2ad4fae27f40056322a13ac7c14778c7be797b
Patricia-gl <patricia.gonzalez2101@alumnos.ubibio.cl>
2025-07-22 19:23:51 -0400

    botón de me gusta en la receta

--- backend/routes/recipeRoutes.js
+++ backend/routes/recipeRoutes.js
@@ -1,15 +1,23 @@
 const express = require('express');
 const router = express.Router();
 const recipeController = require('../controllers/recipeController');
-
+const verificarToken = require('../middleware/authMiddleware');
+console.log('🧪 verificarLike existe:', typeof recipeController.verificarLike);
 router.get('/buscar', recipeController.buscarRecipe);
-
 router.get('/categorias', recipeController.getCategorias);
-
+//router.get('/favoritos/:id_usuario', verificarToken, recipeController.obtenerFavoritosPorUsuario);
+router.get('/:id/like', verificarToken, recipeController.verificarLike); // Verifica estado
+router.post('/:id/like', verificarToken, recipeController.darLike);
+router.delete('/:id/like', verificarToken, recipeController.quitarLike);
+// Obtener receta por ID
+router.get('/:id', recipeController.getRecipeById);
 // Obtener todas las recetas
 router.get('/', recipeController.getRecipes);
 
-// Obtener receta por ID
-router.get('/:id', recipeController.getRecipeById);
+
+
+
+
 
 module.exports = router;
+
